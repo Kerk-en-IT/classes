00f826912afcaf62a2be8da74677aad6c00b47b4
Kerk en IT <info@kerkenit.nl>
2026-06-10 13:31:53 +0200

    Update class.Format.php

--- src/class.Format.php
+++ src/class.Format.php
@@ -436,11 +436,9 @@ class Format
 	 */
 	public static function is_uuid($uuid)
 	{
-
-		if (!is_string($uuid) || (preg_match('/^[a-f\d]{8}(-[a-f\d]{4}){4}[a-f\d]{8}$/i', strtolower($uuid)) !== 1)) {
+		if (empty($uuid) || !is_string($uuid) || (preg_match('/^[a-f\d]{8}(-[a-f\d]{4}){4}[a-f\d]{8}$/i', strtolower($uuid)) !== 1)) {
 			return false;
 		}
-
 		return true;
 	}
 
