eae8c81951cf452f998525be335d157e8cc3ea97
Kerk en IT <info@kerkenit.nl>
2026-09-07 11:07:24 +0200

    Create global.php

---
+++ src/global.php
@@ -0,0 +1,44 @@
+<?php
+use \KerkEnIT;
+
+
+if (!function_exists('varDump')) :
+	/**
+	 * var_dump all content
+	 *
+	 * @param  mixed $params
+	 * @return void
+	 */
+	function varDump(...$params)
+	{
+		\KerkEnIT\varDump($params);
+	}
+endif;
+
+if (!function_exists('varDie')) :
+	function varDie(...$params)
+	{
+		\KerkEnIT\varDie($params);
+	}
+endif;
+
+if (!function_exists('log')) :
+	/**
+	 * var_dump all content
+	 *
+	 * @param  mixed $params
+	 * @return void
+	 */
+	function log(...$params)
+	{
+		\KerkEnIT\log($params);
+	}
+endif;
+
+if (!function_exists('error')) :
+	function error(...$params)
+	{
+		\KerkEnIT\error($params);
+	}
+endif;
+?>
